cddaf043c4686d25566d5c8acfb55b9d57dd871f
FloWambui <gflorencewambui@gmail.com>
2022-05-30 12:32:06 +0300

    add the images model

--- picasso/models.py
+++ picasso/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Category(models.Model):
@@ -38,3 +39,52 @@ class Location(models.Model):
     @classmethod
     def display_all_location(cls):
         return cls.objects.all()
+
+class Image(models.Model):
+    #image field
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    category=models.ForeignKey('Category', on_delete=models.CASCADE)
+    location=models.ForeignKey('Location', on_delete=models.CASCADE)
+
+    def save_image(self):
+        self.save()
+    
+    @classmethod
+    def filter_by_location(cls, location):
+        images = Image.objects.filter(location__name=location)
+        return images    
+
+    def update_image(self, name, description, location, category):
+        self.name = name
+        self.description = description
+        self.location = location
+        self.category = category
+        self.save()
+
+    @classmethod
+    def get_all_images(cls):
+        images = Image.objects.all()
+        return images
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = Image.objects.get(id=id)
+        return image
+
+    @classmethod
+    def filter_by_category(cls, category):
+        images = Image.objects.filter(category__name=category)
+        return images
+
+    
+    
+    def __str__(self):
+        return self.name
+
+
+
+    def delete_image(self):
+        self.delete()
