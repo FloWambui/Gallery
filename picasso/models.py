267f43b259530205fb8beff58cb974026b38cfd8
FloWambui <gflorencewambui@gmail.com>
2022-05-30 08:50:05 +0300

    add the categiries model

--- picasso/models.py
+++ picasso/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=50, unique=True), 
+
+    def save_category(self):
+        self.save()
+
+
+    def update_category(self, name):
+        self.name = name
+        self.save()
+
+    
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
